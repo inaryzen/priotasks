27aded3ffb8cb5290ae3235ee0510413a09acade
inaryzen <inaryzen@gmail.com>
2025-02-16 13:59:38 +0100

    refactor settings

--- db/dbsqlite.go
+++ db/dbsqlite.go
@@ -2,15 +2,11 @@ package db
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	"log"
 	"path/filepath"
-	"time"
 
 	"github.com/inaryzen/priotasks/common"
-	"github.com/inaryzen/priotasks/consts"
-	"github.com/inaryzen/priotasks/models"
 	_ "modernc.org/sqlite"
 )
 
@@ -21,10 +17,6 @@ type DbSQLite struct {
 func NewDbSQLite() *DbSQLite {
 	return &DbSQLite{}
 }
-
-const (
-	SETTINGS_COLUMNS = "id, filter_completed, filter_incompleted, active_sort_column, active_sort_direction, completed_from, completed_to"
-)
 
 func (d *DbSQLite) Init() {
 	dir, err := common.ResolveAppDir()
@@ -40,25 +32,7 @@ func (d *DbSQLite) Init() {
 	d.instance = db
 
 	d.initTasks()
-
-	_, err = d.instance.Exec(`
-		CREATE TABLE IF NOT EXISTS settings (
-			id TEXT PRIMARY KEY,
-			filter_completed BOOLEAN,
-			filter_incompleted BOOLEAN,
-			active_sort_column INTEGER,
-			active_sort_direction INTEGER,
-			completed_from TEXT,
-			completed_to TEXT
-		);
-	`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	d.addSettingsCompletedFrom()
-	d.addSettingsCompletedTo()
-	d.addSettingsFilterIncomplete()
+	d.initSettings()
 }
 
 func (d *DbSQLite) columnExists(tableName, columnName string) bool {
@@ -86,113 +60,7 @@ func (d *DbSQLite) columnExists(tableName, columnName string) bool {
 	return false
 }
 
-func (d *DbSQLite) addSettingsCompletedFrom() {
-	if !d.columnExists("settings", "completed_from") {
-		_, err := d.instance.Exec("ALTER TABLE settings ADD COLUMN completed_from TEXT DEFAULT '" + models.NOT_COMPLETED.Format(consts.DEFAULT_DATE_FORMAT) + "'")
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func (d *DbSQLite) addSettingsCompletedTo() {
-	if !d.columnExists("settings", "completed_to") {
-		_, err := d.instance.Exec("ALTER TABLE settings ADD COLUMN completed_to TEXT DEFAULT '" + models.NOT_COMPLETED.Format(consts.DEFAULT_DATE_FORMAT) + "'")
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func (d *DbSQLite) addSettingsFilterIncomplete() {
-	if !d.columnExists("settings", "filter_incompleted") {
-		_, err := d.instance.Exec("ALTER TABLE settings ADD COLUMN filter_incompleted BOOLEAN DEFAULT 0")
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
 func (d *DbSQLite) Close() {
 	common.Debug("closing db...")
 	d.instance.Close()
-}
-
-func (d *DbSQLite) FindSettings(settingsId string) (models.Settings, error) {
-	var settings models.Settings
-	var completedFrom, completedTo string
-
-	row := d.instance.QueryRow("SELECT "+SETTINGS_COLUMNS+" FROM settings WHERE id = ?", settingsId)
-	err := row.Scan(
-		&settings.Id,
-		&settings.TasksQuery.FilterCompleted,
-		&settings.TasksQuery.FilterIncompleted,
-		&settings.TasksQuery.SortColumn,
-		&settings.TasksQuery.SortDirection,
-		&completedFrom,
-		&completedTo,
-	)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return models.Settings{}, ErrNotFound
-		}
-		return models.Settings{}, fmt.Errorf("failed to fetch settings: %s: %w", settingsId, err)
-	}
-
-	if completedFrom == "" {
-		completedFrom = time.Time{}.Format(consts.DEFAULT_DATE_FORMAT)
-	}
-	settings.TasksQuery.CompletedFrom, err = time.Parse(consts.DEFAULT_DATE_FORMAT, completedFrom)
-	if err != nil {
-		return models.Settings{}, fmt.Errorf("failed to parse completed_from: %w", err)
-	}
-
-	if completedTo == "" {
-		completedTo = time.Time{}.Format(consts.DEFAULT_DATE_FORMAT)
-	}
-	settings.TasksQuery.CompletedTo, err = time.Parse(consts.DEFAULT_DATE_FORMAT, completedTo)
-	if err != nil {
-		return models.Settings{}, fmt.Errorf("failed to parse completed_to: %w", err)
-	}
-
-	return settings, nil
-}
-
-func (d *DbSQLite) SaveSettings(s models.Settings) error {
-	sqlQuery :=
-		"INSERT INTO settings (" + SETTINGS_COLUMNS + ") " +
-			`VALUES (?, ?, ?, ?, ?, ?, ?)
-        ON CONFLICT(id) DO UPDATE SET
-            filter_completed=excluded.filter_completed,
-            filter_incompleted=excluded.filter_incompleted,
-            active_sort_column=excluded.active_sort_column,
-            active_sort_direction=excluded.active_sort_direction,
-            completed_from=excluded.completed_from,
-            completed_to=excluded.completed_to
-    `
-	completedFrom := time.Time{}.Format(consts.DEFAULT_DATE_FORMAT)
-	if !s.TasksQuery.CompletedFrom.IsZero() {
-		completedFrom = s.TasksQuery.CompletedFrom.Format(consts.DEFAULT_DATE_FORMAT)
-	}
-
-	completedTo := time.Time{}.Format(consts.DEFAULT_DATE_FORMAT)
-	if !s.TasksQuery.CompletedTo.IsZero() {
-		completedTo = s.TasksQuery.CompletedTo.Format(consts.DEFAULT_DATE_FORMAT)
-	}
-
-	args := []interface{}{
-		s.Id,
-		s.TasksQuery.FilterCompleted,
-		s.TasksQuery.FilterIncompleted,
-		s.TasksQuery.SortColumn,
-		s.TasksQuery.SortDirection,
-		completedFrom,
-		completedTo,
-	}
-
-	_, err := d.instance.Exec(sqlQuery, args...)
-	if err != nil {
-		return fmt.Errorf("failed to save settings: %v: %w", s, err)
-	}
-	return nil
 }
--- go.mod
+++ go.mod
@@ -4,27 +4,17 @@ go 1.23.2
 
 require (
 	github.com/a-h/templ v0.3.833
-	github.com/dgraph-io/badger/v4 v4.5.1
 	github.com/google/uuid v1.6.0
 	modernc.org/sqlite v1.34.5
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/dgraph-io/ristretto/v2 v2.1.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
-	github.com/google/flatbuffers v25.1.24+incompatible // indirect
-	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c // indirect
-	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
-	google.golang.org/protobuf v1.36.4 // indirect
 	modernc.org/libc v1.61.11 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.8.2 // indirect
